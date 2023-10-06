d8cf395c38acf8a6ab03a47c28fb4b92f6fac8dc
Sam Ford <samueljamesford@googlemail.com>
2023-10-06 19:39:07 +0100

    latest commits

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from lib.database_connection import DatabaseConnection
 from lib.ProductRepo import *
+from lib.OrderRepo import *
 
 class Application():
     def __init__(self):
@@ -21,10 +22,18 @@ class Application():
         product_repo = ProductRepo(self._connection)
         product_repo.check_quantity(name)
 
-        
-        
+    def check_order(self,order_id):
+        order_repo = OrderRepo(self._connection)
+        order_repo.find_order(order_id)
 
+# Order Number: 5
+# Customer Name: Ned Stark
+# Order Placed: 10/01/2023
+# Ordered: 
+# 1X Baked Beans
+# 5X Diamonds
+        
+# Ned Stark - Placed on - Order Number
 
 Trial=Application()
-Trial.return_all_products()
-Trial.check_stock_level('Diamonds')
+Trial.check_order(2)
--- lib/OrderRepo.py
+++ lib/OrderRepo.py
@@ -1,7 +1,18 @@
 from lib.orderclass import *
+import datetime
+
 
 class OrderRepo:
     def __init__(self,connection):
         self.connection=connection
+
+    def find_order(self,order_id):
+        rows = self.connection.execute('SELECT orders.customer_name,orders.order_date,products.name FROM orders_products JOIN orders ON orders_products.order_id = orders.id JOIN products ON orders_products.product_id = products.id where order_id = %s',[order_id])
+        # products = []
+        # for row in rows:
+        #     product = Product(row["id"], row["name"], row["unit_price"],row["quantity"])
+        #     products.append(product)
+        # return products
+        print (rows[0]["order_date"].strftime("%d-%m-%Y"))
 
     
